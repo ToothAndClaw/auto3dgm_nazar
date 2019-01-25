25b11b8193b4776c5fa3c27512486cd957f2805b
Brandon Guo <blhguo@gmail.com>
2019-01-25 16:19:03 -0500

    updated per critiques

--- auto3dgm/mesh/Tester.py
+++
@@ -1,8 +0,0 @@
-from meshfactory import MeshFactory
-print("Begin Test")
-
-factory = meshfactory.MeshFactory()
-x = factory.mesh_from_file("test.ply")
-print("This is mesh"), x
-print("This is mesh.faces"), x.faces
-print("This is mesh.vetices"), x.vertices

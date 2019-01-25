38fa96a983e918eb535c829c258e6d1fa3ca7b4f
Brandon Guo <blhguo@gmail.com>
2019-01-25 14:12:07 -0500

    Rotation functionality is working as far as I can tell, it's hard to demonstrate graphically

--- auto3dgm/mesh/mesh.py
+++ auto3dgm/mesh/mesh.py
@@ -61,7 +61,7 @@ class Mesh:
     def centroid2(self):
         centerFilter = vtk.vtkCenterOfMass()
         centerFilter.SetInputData(self.polydata)
-        centerFilter.SetUseScalarAsWeights(False)
+        centerFilter.SetUseScalarsAsWeights(False)
         centerFilter.update()
         return centerFilter.GetCenter()
 
@@ -69,21 +69,20 @@ class Mesh:
     def scale(self):
         return np.linalg.norm(vtk_to_numpy(self.polydata.GetPoints().GetData()), 'fro')
 
-    @property
     def rotate(self, arr):
         temp = isValidRotation(arr)
         assert(temp == True)
-        sy = math.sqrt(arr[0, 0] * arr[0, 0] + arr[1, 0] * arr[1, 0])
+        sy = math.sqrt(arr[0][0] * arr[0][0] + arr[1][0] * arr[1][0])
         singular = sy < 1e-6
         x, y, z = 0, 0, 0
         if not singular :
-            x = math.atan2(arr[2, 1], arr[2, 2])
-            y = math.atan2(-arr[2, 0], sy)
-            z = math.atan2(arr[1, 0], arr[0, 0])
+            x = math.atan2(arr[2][1], arr[2][2])
+            y = math.atan2(-arr[2][0], sy)
+            z = math.atan2(arr[1][0], arr[0][0])
 
         else :
-            x = math.atan2(-arr[1, 2], arr[1, 1])
-            y = math.atan2(-arr[2, 0], sy)
+            x = math.atan2(-arr[1][2], arr[1][1])
+            y = math.atan2(-arr[2][0], sy)
             z = 0
 
         rpy = np.array([x, y, z])
@@ -107,7 +106,7 @@ class Mesh:
 def isValidRotation(arr):
     rt = np.transpose(arr)
     identity = np.dot(rt, arr)
-    I = np.identity(3, dtype = arr.dtype)
+    I = np.identity(3)
     n = np.linalg.norm(I - identity)
     return n < 1e-6
 

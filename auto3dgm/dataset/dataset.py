f50c3e6924e93fdf3d7a81e31c2f2d7b31338079
hkirvesl <43118971+hkirvesl@users.noreply.github.com>
2019-02-01 11:00:03 -0500

    Dataset class updated as per the design document

--- auto3dgm/dataset/dataset.py
+++ auto3dgm/dataset/dataset.py
@@ -1,24 +1,13 @@
 class Dataset:
-    #params: self,
-    #meshes: either a mesh object or a list or a dictionary of mesh objects
-    #analyses: an analysis object (list, dictionary of such) associated with the mesh objects
-    def __init__(self, meshes, analyses={}):
-        if isinstance(meshes,dict):
-            mesh_set=meshes
-            if isinstance(analyses,dict):
-                analysis_set=analyses
-        elif isinstance(meshes, (list,)):
-            ID=0
-            self.mesh_set={}
-            self.analysis_set={}
-            for i in range(len(meshes)):
-                ID=ID+1
-                self.mesh_set[ID]=meshes[i]
-                if (isinstance(analyses, (list,)) and len(meshes)==len(analyses)): 
-                    self.analysis_set[ID]=analyses[i]
+    #params: self
+    #meshes: a list of mesh objects or a mesh object
+    def __init__(self, meshes):
+        if(isinstance(meshes, list)):
+            self.meshes=meshes
         elif hasattr(meshes,'vertices'):
-            self.mesh_set={1:meshes}
-            self.analysis_set={1:analyses} 
+            meshlist=[]
+            meshlist.append(meshes)
+            self.meshes=meshlist 
         else:
             msg = 'Input not a list nor a valid mesh object'
             raise OSError(msg)
